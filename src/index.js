a55c70028672b64f44b83db8a8f6672755495e20
skentala1 <135027565+skentala@users.noreply.github.com>
2023-06-15 14:55:02 +0000

    Update 34 files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./styles.css";
 
+//https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326
+
+async function fetchData() {
+    const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+    const res = await fetch(url);
+    const data = await res.json();
+    
+    let map = L.map('map', {
+        minZoom: -3
+    })
+    
+    let geoJson = L.geoJSON(data, {
+        onEachFeature: getFeature,
+        style: getStyle
+    }).addTo(map)
+}
+
